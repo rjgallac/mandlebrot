cf9dc1c1aaf8f11f8c2430a996e35f9e0084b83b
rob <rjgallac@hotmail.com>
2022-08-14 20:45:10 +0100

    basic working mandlebrot

---
+++ main.js
@@ -0,0 +1,43 @@
+colors = {
+    1: '#111',
+    2: '#333',
+    3: '#555',
+    4: '#777',
+    5: '#999',
+    6: '#aaa',
+    7: '#ccc',
+    8: '#eee',
+    0: '#fff'
+}
+
+function drawPixel(context, x, y, color) {
+    var roundedX = Math.round(x);
+    var roundedY = Math.round(y);
+    context.fillStyle = color;
+    context.fillRect(roundedX, roundedY, 1, 1);
+}
+
+
+var canvas = document.querySelector('#myCanvas');
+var context = canvas.getContext('2d')
+
+for (i=0; i<canvas.width; i++) {
+    for (j=0; j<canvas.height; j++) {
+        let xdiv = Math.abs(-2.00 - 0.47) / canvas.width;
+        x0 = xdiv * i - 2;
+        let ydiv = Math.abs(-1.12 - 1.12) / canvas.height;
+        y0 = ydiv * j - 1.12;
+        x=0.0;
+        y=0.0;
+        iteration = 0;
+        max_iterations = 24;
+        while(x*x + y*y <= 2*2 && iteration < max_iterations){
+            xtemp = x*x - y*y + x0
+            y = 2*x*y + y0
+            x = xtemp
+            iteration++;
+        }
+        drawPixel(context, i, j, colors[iteration % 8]);
+    
+    }
+}
